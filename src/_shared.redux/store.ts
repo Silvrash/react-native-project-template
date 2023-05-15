529f91ee589d0495e767c50a30eb1b7d5e1376b6
Benjamin Arko Afrasah <barkoafrasah@gmail.com>
2023-05-15 20:32:56 +0000

    feat: setup redux persist

--- src/_shared.redux/store.ts
+++ src/_shared.redux/store.ts
@@ -1,11 +1,22 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { configureStore } from '@reduxjs/toolkit';
-import { persistReducer, persistStore } from 'redux-persist';
+import {
+	FLUSH,
+	PAUSE,
+	PERSIST,
+	PURGE,
+	PersistConfig,
+	REGISTER,
+	REHYDRATE,
+	persistReducer,
+	persistStore,
+} from 'redux-persist';
 import rootReducer from './reducers';
 
-const persistConfig = {
+const persistConfig: PersistConfig<ReturnType<typeof rootReducer>> = {
 	key: 'root',
 	storage: AsyncStorage,
+	version: 1,
 	blacklist: ['volatile'],
 };
 
@@ -13,6 +24,12 @@ const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const store = configureStore({
 	reducer: persistedReducer,
+	middleware: getDefaultMiddleware =>
+		getDefaultMiddleware({
+			serializableCheck: {
+				ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+			},
+		}),
 });
 
 export const persistor = persistStore(store);
--- src/screen.example/Example.tsx
+++ src/screen.example/Example.tsx
@@ -1,13 +1,26 @@
-import { Flex } from 'native-base';
+import { Button, Center, Flex, Text } from 'native-base';
 import AppBar from 'src/_shared.components/AppBar';
+import { decrement, increment, selectCount } from 'src/_shared.redux/reducers/countSlice';
+import { useAppDispatch, useAppSelector } from 'src/_shared.redux/types';
 
 const Example = () => {
-	// const count = useAppSelector(selectCount);
-	// const dispatch = useAppDispatch();
+	const count = useAppSelector(selectCount);
+	const dispatch = useAppDispatch();
 
 	return (
 		<Flex flex="1" bgColor={'#FAFAFA'} safeAreaTop>
 			<AppBar title="Example" />
+			<Center flex="1">
+				<Text>{count}</Text>
+				<Button.Group mt="2">
+					<Button size="xs" onPress={() => dispatch(increment())}>
+						+
+					</Button>
+					<Button size="xs" onPress={() => dispatch(decrement())}>
+						-
+					</Button>
+				</Button.Group>
+			</Center>
 		</Flex>
 	);
 };
